8f74ec18be5d14d1a1c4700437300368609f7150
Randall Lashley <rlashley415@gmail.com>
2019-09-19 16:40:00 -0500

    Cleaned up code, created proper server loop
    Cleaned up formatting, refactored to create proper server execution loop. Has conditions for continuous running and shut down paths

--- Server.java
+++ Server.java
@@ -13,85 +13,90 @@ import java.io.*;
 
 public class Server {
 
-    ServerSocket serverSocket = null;
-    Socket link = null;
-    Scanner input = null;
-    PrintWriter output = null; //Sends output from server to client app
-    String rawStringToParse = ""; //String that stores raw data first pulled from feedback.txt file
+	ServerSocket serverSocket = null;
+	Socket link = null;
+	Scanner input = null;
+	PrintWriter output = null; //Sends output from server to client app
+	String rawStringToParse = ""; //String that stores raw data first pulled from feedback.txt file
+	bool keepRunning = true; //Variable to trigger shutdown of server
     
 	public static void main(String[] args) throws Exception {
 
 		//Create Server class
-        Server server = new Server();
-        
-        //This will be used to test client locally, removed later to be replaced with outside app.
-        Client client = new Client();
+        	Server server = new Server();
+		//This will be used to test client locally, removed later to be replaced with outside app.
+        	Client client = new Client();
+	
+		//Wait for initial connection
+        	try {
+        		serverSocket = new ServerSocket(port);
+        	} 
+        	catch (IOException e) {
+        		System.err.println("Could not listen on port: 3200. Error: " + e);
+			System.exit(1);
+        	}
 
-        //Launch server
-        server.start(3200);
+        	//Launch server
+		do{
+        	server.start(3200);
+		} while (keepRunning); //Need to create condition where variable switches to turn off server
         
-        //Listen for handshake, when one arrives, take user name and password in encrypted format.
-        
-        
-        //If user name and pass match, send back connection made message
-        
-        
-        //Read in feedback txt file.       
-        server.rawStringToParse = server.readTextFile();
-        
-        //Send to connected device
-        server.output.println(server.rawStringToParse);
-        
-        //close connection when input key 47 arrives
-		if(Integer.parseInt(input)==47){
-			server.stop();
-		}
+		server.stop();       
 	}
 	
 	//This method starts the server, makes connections
-    public void start(int port) throws Exception{   	
-        try {
-        	serverSocket = new ServerSocket(port);
-        } 
-        catch (IOException e) {
-        	System.err.println("Could not listen on port: 3200. Error: " + e);
-        }
+	public void start(int port) throws Exception{   	
         
-        try {
-        	link = serverSocket.accept();
-        }
-        catch(IOException e) {
-        	System.err.println("Connection issue on serverSocket. Error: " + e);
-        }             
+        	try {
+        		link = serverSocket.accept();
+        	}
+        	catch(IOException e) {
+        		System.err.println("Connection issue on serverSocket. Error: " + e);
+			System.exit(1);
+        	}             
         
-        input = new Scanner(link.getInputStream());
-        output = new PrintWriter(link.getOutputStream(),true);
-        System.out.println("Server started, waiting on port 3200. Input and Output ready");
-    }
+        	input = new Scanner(link.getInputStream()); //incoming data to server
+        	output = new PrintWriter(link.getOutputStream(),true); //outgoing data to client on other side
+        	System.out.println("Server started, waiting on port 3200. Input and Output ready");
+	    
+		//Listen for handshake
+        	if(input!=null){
+		//When data arrives, take user name and password in encrypted format.
+		
+        	//If user name and pass match, send back connection made message  
+	
+		//Read in feedback txt file.       
+        	server.rawStringToParse = server.readTextFile();
+		
+		//Send to connected device
+        	server.output.println(server.rawStringToParse);
+		}
+    	}
     
-    //This method reads in the text file
-    public String readTextFile() throws IOException {
-    	File file = new File("C:\feedback.txt");
+	//This method reads in the text file
+	public String readTextFile() throws IOException {
+    		File file = new File("C:\feedback.txt");
     	
-    	BufferedReader reader = new BufferedReader(new FileReader(file));
-    	String documentText = "";
-    	try {
+    		BufferedReader reader = new BufferedReader(new FileReader(file));
+    		String documentText = "";
+    		try {
 			while(reader.readLine() != null) {
 				documentText+=reader.readLine();
 			}
-		} catch (IOException e) {
-			System.out.println(e);
-		}
-    	reader.close();
-    	return documentText;
-    }
+			} catch (IOException e) {
+				System.out.println(e);
+			}
+    		reader.close();
+    		return documentText;
+    	}
     
-    //This method stops the server
+    	//This method stops the server
 	public void stop() throws Exception {
 		input.close();
 		output.close();
-	    link.close();
-	    serverSocket.close();
+		link.close();
+		serverSocket.close();
+		System.exit(1);
 	}
 }
 
